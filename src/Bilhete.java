4dd3f2a1817e3a72143c698ad105e9ba2c34c0cf
M1ngos <mingoemanuel07@gmail.com>
2021-07-13 06:21:00 +0200

    Classe Bilhete

--- src/Bilhete.java
+++ src/Bilhete.java
@@ -1,3 +1,61 @@
-public class Bilhete {
-    public
+public abstract class Bilhete {
+
+    protected String nomeCliente,bi_Cliente,data_partida,data_chegada,formaPagamento;
+    protected short codigo;
+    protected float valor;
+
+    public Bilhete(String nomeCliente, String bi_Cliente, String data_partida, String data_chegada, String formaPagamento, short codigo, float valor) {
+        this.nomeCliente = nomeCliente;
+        this.bi_Cliente = bi_Cliente;
+        this.data_partida = data_partida;
+        this.data_chegada = data_chegada;
+        this.formaPagamento = formaPagamento;
+        this.codigo = codigo;
+        this.valor = valor;
+    }
+
+    public Bilhete() {
+        this("","","","","",(short) 0,(float) 0);
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public void setBi_Cliente(String bi_Cliente) {
+        this.bi_Cliente = bi_Cliente;
+    }
+
+    public void setData_partida(String data_partida) {
+        this.data_partida = data_partida;
+    }
+
+    public void setData_chegada(String data_chegada) {
+        this.data_chegada = data_chegada;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public void setCodigo(short codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Bilhete{" +
+                "nomeCliente='" + nomeCliente + '\'' +
+                ", bi_Cliente='" + bi_Cliente + '\'' +
+                ", data_partida='" + data_partida + '\'' +
+                ", data_chegada='" + data_chegada + '\'' +
+                ", formaPagamento='" + formaPagamento + '\'' +
+                ", codigo=" + codigo +
+                ", valor=" + valor +
+                '}';
+    }
 }
